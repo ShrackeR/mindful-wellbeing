8ed8217464d3452099788ddf6ef16a1f5e931150
roze-ferns <rozeeta1107@gmail.com>
2023-09-18 02:55:30 +0530

    18 sept

--- frontend/src/pages/Api.js
+++ frontend/src/pages/Api.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './Api.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Api.css";
+import Navigation from "../components/Navigation";
 
 const Api = () => {
   const [exercises, setExercises] = useState([]);
-  const [activity, setActivity] = useState('');
-  const [level, setLevel] = useState('');
+  const [activity, setActivity] = useState("");
+  const [level, setLevel] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [selectedExercise, setSelectedExercise] = useState(null);
 
@@ -19,8 +20,8 @@ const Api = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Selected Activity:', activity);
-    console.log('Selected Level:', level);
+    console.log("Selected Activity:", activity);
+    console.log("Selected Level:", level);
 
     if (activity && level) {
       setSubmitted(true);
@@ -30,15 +31,15 @@ const Api = () => {
 
   const fetchData = async () => {
     const options = {
-      method: 'GET',
-      url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises',
+      method: "GET",
+      url: "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises",
       params: {
         type: activity,
         difficulty: level,
       },
       headers: {
-        'X-RapidAPI-Key': '830abd8e42msh8137e948f0b199fp1b45ecjsn13fdaf90a884',
-        'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com',
+        "X-RapidAPI-Key": "830abd8e42msh8137e948f0b199fp1b45ecjsn13fdaf90a884",
+        "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com",
       },
     };
 
@@ -58,93 +59,100 @@ const Api = () => {
   };
 
   return (
-    <div className="page-container">
-    <div className="container">
-      <h2>Exercise Finder</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="activity">Select an Activity:</label>
-          <select
-            id="activity"
-            name="activity"
-            className="form-control"
-            value={activity}
-            onChange={handleActivityChange}
-          >
-            <option value="">Select an activity</option>
-            <option value="cardio">Cardio</option>
-            <option value="olympic_weightlifting">Olympic Weightlifting</option>
-            <option value="plyometrics">Plyometrics</option>
-            <option value="powerlifting">Powerlifting</option>
-            <option value="strength">Strength</option>
-            <option value="stretching">Stretching</option>
-            <option value="strongman">Strongman</option>
-          </select>
-        </div>
+    <>
+      <Navigation />
+      <div className="page-container">
+        <div className="container">
+          <h2>Exercise Finder</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="activity">Select an Activity:</label>
+              <select
+                id="activity"
+                name="activity"
+                className="form-control"
+                value={activity}
+                onChange={handleActivityChange}
+              >
+                <option value="">Select an activity</option>
+                <option value="cardio">Cardio</option>
+                <option value="olympic_weightlifting">
+                  Olympic Weightlifting
+                </option>
+                <option value="plyometrics">Plyometrics</option>
+                <option value="powerlifting">Powerlifting</option>
+                <option value="strength">Strength</option>
+                <option value="stretching">Stretching</option>
+                <option value="strongman">Strongman</option>
+              </select>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="level">Select a Level:</label>
-          <select
-            id="level"
-            name="level"
-            className="form-control"
-            value={level}
-            onChange={handleLevelChange}
-          >
-            <option value="">Select a level</option>
-            <option value="beginner">Beginner</option>
-            <option value="intermediate">Intermediate</option>
-            <option value="expert">Expert</option>
-          </select>
-        </div>
+            <div className="form-group">
+              <label htmlFor="level">Select a Level:</label>
+              <select
+                id="level"
+                name="level"
+                className="form-control"
+                value={level}
+                onChange={handleLevelChange}
+              >
+                <option value="">Select a level</option>
+                <option value="beginner">Beginner</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="expert">Expert</option>
+              </select>
+            </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
 
-      {submitted && (
-        <div>
-          <h3>Exercise Details</h3>
-          <div className="exercise-cards">
-            {exercises.map((exercise, index) => (
-              <div className="exercise-card" key={index}>
-                <h4>{exercise.name}</h4>
-                <p>
-                  <strong>Type:</strong> {exercise.type}
-                </p>
-                <p>
-                  <strong>Difficulty:</strong> {exercise.difficulty}
-                </p>
-                <p>
-                  <strong>Equipment:</strong> {exercise.equipment}
-                </p>
-                <p>
-                <strong>Muscle:</strong> {exercise.muscle}
-                </p>
-                <button
-  className={`button-view-instructions ${
-    selectedExercise === exercise ? 'active' : ''
-  }`}
-  onClick={() => handleViewInstructions(exercise)}
->
-  {selectedExercise === exercise ? 'Hide Instructions' : 'View Instructions'}
-</button>
-
-                {selectedExercise === exercise && (
-                  <div className="instructions">
+          {submitted && (
+            <div>
+              <h3>Exercise Details</h3>
+              <div className="exercise-cards">
+                {exercises.map((exercise, index) => (
+                  <div className="exercise-card" key={index}>
+                    <h4>{exercise.name}</h4>
                     <p>
-                      <strong>Instructions:</strong> {exercise.instructions}
+                      <strong>Type:</strong> {exercise.type}
                     </p>
+                    <p>
+                      <strong>Difficulty:</strong> {exercise.difficulty}
+                    </p>
+                    <p>
+                      <strong>Equipment:</strong> {exercise.equipment}
+                    </p>
+                    <p>
+                      <strong>Muscle:</strong> {exercise.muscle}
+                    </p>
+                    <button
+                      className={`button-view-instructions ${
+                        selectedExercise === exercise ? "active" : ""
+                      }`}
+                      onClick={() => handleViewInstructions(exercise)}
+                    >
+                      {selectedExercise === exercise
+                        ? "Hide Instructions"
+                        : "View Instructions"}
+                    </button>
+
+                    {selectedExercise === exercise && (
+                      <div className="instructions">
+                        <p>
+                          <strong>Instructions:</strong> {exercise.instructions}
+                        </p>
+                      </div>
+                    )}
                   </div>
-                )}
+                ))}
               </div>
-            ))}
-          </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-    </div>
+      </div>
+    </>
   );
 };
 
